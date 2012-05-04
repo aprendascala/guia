b58fdd2bb140245bfbc3ce3bd7beb50b94a0c320
Jonas Abreu <jonas@vidageek.net>
2012-05-04 19:17:50 -0300

    resolvendo exercicios de operacoes

--- src/test/scala/aprendascala/ex01/operacoes/Ex01OperacoesBasicas.scala
+++ src/test/scala/aprendascala/ex01/operacoes/Ex01OperacoesBasicas.scala
@@ -11,27 +11,27 @@ class Ex01OperacoesBasicas extends Exercicios {
   """
 
   exercicio("Somar Dois Numeros") {
-    1 MUDE_AQUI 1 deveSer 2
-    2 MUDE_AQUI 2 deveSer 4
+    1 + 1 deveSer 2
+    2 + 2 deveSer 4
   }
 
   exercicio("Subtrair Dois Numeros") {
-    1 MUDE_AQUI 1 deveSer 0
-    2 MUDE_AQUI 3 deveSer -1
+    1 - 1 deveSer 0
+    2 - 3 deveSer -1
   }
 
   exercicio("Multiplicar Dois Numeros") {
-    1 MUDE_AQUI 1 deveSer 1
-    2 MUDE_AQUI 3 deveSer 6
+    1 * 1 deveSer 1
+    2 * 3 deveSer 6
   }
 
   exercicio("Dividir Dois Numeros") {
-    1 MUDE_AQUI 1 deveSer 1
-    4 MUDE_AQUI 2 deveSer 2
+    1 / 1 deveSer 1
+    4 / 2 deveSer 2
   }
 
   exercicio("Resto da Divisao") {
-    4 MUDE_AQUI 2 deveSer 0
-    8 MUDE_AQUI 7 deveSer 1
+    4 % 2 deveSer 0
+    8 % 7 deveSer 1
   }
 }
